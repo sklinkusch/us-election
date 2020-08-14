25278ede65aa5b5e300d6859b3b8b4983c3fee4a
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2020-08-14 17:34:17 +0200

    add full path to files in presprob-pc.pl

--- presprob-pc.pl
+++ presprob-pc.pl
@@ -5,6 +5,7 @@ use warnings;
 use Algorithm::Combinatorics qw/variations_with_repetition/;
 use List::Util qw(first);
 use Math::BigFloat;
+use Cwd 'abs_path';
 use FindBin;
 use lib $FindBin::RealBin;
 use Elections qw(get_statevals get_parstates check_par check_states get_openstates get_safeval get_swingperm commify sort_states delete_mainenebraska build_mat result_mat get_statedesc get_head build_permas modify_maine modify_nebraska print_info get_nome modify_results delete_doubles get_percent closed_simple build_matpc commify_float);
@@ -15,8 +16,10 @@ if ($#ARGV < 1){
 	print "percent file contains dem/rep probability for all states/districts\n";
 	exit;
 }
-my $datfile = $ARGV[0];
-my $percfile = $ARGV[1];
+my $datfile = join('',$ARGV[0]);
+my $percfile = join('',$ARGV[1]);
+my $fulldat = abs_path($datfile);
+my $fullperc = abs_path($percfile);
 
 # get the states and the respective number of electors
 my %statevals = get_statevals();
@@ -24,13 +27,13 @@ my %statevals = get_statevals();
 # get the percentages from percent file
 my %dsperc;
 my %rsperc;
-get_percent($percfile,\%dsperc,\%rsperc);
+get_percent($fullperc,\%dsperc,\%rsperc);
 
 # put the hash keys and values into two arrays (sorted)
 my @allstates = sort { $statevals{$b} <=> $statevals{$a} || $a cmp $b } keys %statevals;
 my @valstates = @statevals{@allstates};
-my @demstates = get_parstates($datfile,'D');
-my @repstates = get_parstates($datfile,'R');
+my @demstates = get_parstates($fulldat,'D');
+my @repstates = get_parstates($fulldat,'R');
 check_states(@demstates,@repstates);
 my @closedstates = closed_simple(\@demstates,\@repstates);
 my @openvalx;
